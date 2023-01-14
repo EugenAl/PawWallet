f247b7e96fe884d67c2f3c45b8dc0c061cc0e233
EugenAl <zheka2142@gmail.com>
2023-01-14 09:50:21 +0300

    project init

---
+++ app/src/main/java/dpr/svich/pawwallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package dpr.svich.pawwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
